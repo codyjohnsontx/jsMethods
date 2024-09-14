d28734d66996672d3fcd9fa526196aa06bd63973
Cody Johnson <codyjohnsontx@gmail.com>
2024-09-14 14:26:30 -0500

    first commit

---
+++ jsMethods.js
@@ -0,0 +1,44 @@
+// Short Give a short description of what the method does, how it works,
+// its time complexity if appropriate, and give
+// three examples of it in action! of
+
+// charAt()
+//
+// Returns a new string consisting of the unit at the specified index in a string
+//
+// Time Complexity is O(1), or constant time
+// This is because strings in javascript are stored as a sequence of characters
+// and accessing a specific character by index doesn't require an iteration over the string
+//
+// Examples:
+// let message = 'hello world'
+// console.log(message.charAt(2))
+// returns l
+
+// charCodeAt()
+
+// concat()
+
+// includes
+
+// indexOf()
+
+// match()
+
+// repeat()
+
+// replace()
+
+// search()
+
+// slice()
+
+// split
+
+// substr
+
+// toLowerCase()
+
+// toUpperCase()
+
+// trim()
